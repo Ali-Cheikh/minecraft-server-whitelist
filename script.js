87485896ebd8578ef95d061e25f6acb8bdfc704d
Ali <alouloucheikh05@gmail.com>
2024-04-17 22:28:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+  }
+document.getElementById('whitelistForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    submitFormToGoogleSheets();
+    document.getElementById('whitelistForm').reset(); // Reset form fields
+    document.getElementById('successMessage').style.display = 'block'; // Show success message
+});
+
+
+function submitFormToGoogleSheets() {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz95y0MUDRFlr0iAAXkceJBXNasGv5EuoQJVzvMhqr9K3dvya-vwPfY14uY2qvoJyS3/exec';
+    const form = document.getElementById('whitelistForm');
+    Swal.fire({
+        icon: 'info',
+        title: 'Submitting...',
+        text: 'Sending whitelist application request',
+        showConfirmButton: false,
+        allowOutsideClick: false
+    });
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            if (response.ok) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success!',
+                    text: `<h3 style="color:white">Your application is submitted.<u> Thank you!</u></h3>`,
+                    timer: 2000,
+                    background: "url(src/download.png)",
+                    timerProgressBar: true,
+                    showConfirmButton: false,
+                    allowOutsideClick: true
+                });
+            } else {
+                throw new Error('Network response was not ok.');
+            }
+        })
+        .catch(error => {
+            console.error('Error!', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong! Please try again later.'
+            });
+        });
+}
+document.addEventListener('scroll', function() {
+    var footerPosition = document.querySelector('footer').getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    if (footerPosition < windowHeight) {
+        document.body.classList.add('footer-reached');
+    } else {
+        document.body.classList.remove('footer-reached');
+    }
+});
